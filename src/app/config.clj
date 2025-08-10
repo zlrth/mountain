18ce94938c91218318e7f9881cd728b5ea6af4e9
Matthew Marks <matthewfmarks@gmail.com>
2025-08-10 19:16:48 -0400

    namespace env vars so i can put them in my .profile

--- src/app/config.clj
+++ src/app/config.clj
@@ -4,9 +4,9 @@
   (or (System/getenv k) default))
 
 (defn cfg []
-  {:db {:jdbc-url (env "DATABASE_URL" "jdbc:postgresql://localhost:5432/mountain?user=postgres&password=postgres")}
-   :postmark {:token (env "POSTMARK_SERVER_TOKEN")
-              :from  (env "POSTMARK_FROM" "no-reply@example.com")
-              :stream (env "POSTMARK_STREAM" "outbound")}
-   :app {:base-url (env "BASE_URL" "http://localhost:3000")
-         :rate-limit-rps (Integer/parseInt (env "WAYBACK_RPS" "1"))}})
+  {:db {:jdbc-url (env "MOUNTAIN_DATABASE_URL" "jdbc:postgresql://localhost:5432/mountain?user=postgres")}
+   :postmark {:token (env "MOUNTAIN_POSTMARK_SERVER_TOKEN")
+              :from  (env "MOUNTAIN_POSTMARK_FROM" "no-reply@example.com")
+              :stream (env "MOUNTAIN_POSTMARK_STREAM" "outbound")}
+   :app {:base-url (env "MOUNTAIN_BASE_URL" "http://localhost:3000")
+         :rate-limit-rps (Integer/parseInt (env "MOUNTAIN_WAYBACK_RPS" "1"))}})
